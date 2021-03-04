18c34922b290231b38b2017ac2c2e2f99975d039
Paulina Ksienzyk <pilnicka8@gmail.com>
2021-03-04 12:21:35 +0100

    Add solution for bonus

---
+++ src/main/java/paulinaksienzyk/com/github/bonus/LambdaFibonacci.java
@@ -0,0 +1,31 @@
+package paulinaksienzyk.com.github.bonus;
+
+import java.util.function.Function;
+import java.util.stream.IntStream;
+
+class LambdaFibonacci {
+
+    //TODO Try to print Fibonacci numbers using streams. Steps:
+    // 1. Implement method fibonacci
+    public static void main(String[] args) {
+
+        IntStream.range(1,9)
+                .boxed()
+                .map(fibonacci())
+                .forEach(System.out::println);
+    }
+
+    private static Function<Integer, Integer> fibonacci() {
+        return new Function<Integer, Integer>() {
+            @Override
+            public Integer apply(Integer x) {
+                if (x < 2) {
+                    return x;
+                } else {
+                    return apply(x-1) + apply(x-2);
+                }
+            }
+        };
+    }
+
+}
