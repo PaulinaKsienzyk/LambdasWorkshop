d7fdc15ca86721630af4f8ba53c42a2740030671
Paulina Ksienzyk <pilnicka8@gmail.com>
2021-03-04 14:58:43 +0100

    Add solution to task3

--- src/main/java/paulinaksienzyk/com/github/task3/FunctionalInterfaces.java
+++ src/main/java/paulinaksienzyk/com/github/task3/FunctionalInterfaces.java
@@ -2,6 +2,7 @@ package paulinaksienzyk.com.github.task3;
 
 import java.util.List;
 import java.util.function.Supplier;
+import java.util.stream.Collectors;
 
 public class FunctionalInterfaces {
 
@@ -15,9 +16,9 @@ public class FunctionalInterfaces {
     // b) Then compose the whole as one Supplier.
     private static void composeSuppliers() {
         WlazlKotek kitty = new WlazlKotek();
-        List<Supplier<String>> partsOfRhyme = List.of();
+        List<Supplier<String>> partsOfRhyme = List.of(kitty::whatAndWhoDid, kitty::whatIsTheSong, kitty::andFinally);
 
-        Supplier<String> wholeRhyme = () -> "correct me";
+        Supplier<String> wholeRhyme = () -> partsOfRhyme.stream().map(Supplier::get).collect(Collectors.joining(", "));
         String result = wholeRhyme.get();
 
         if (!("Wlazl kotek na potek i mruga, ladna to piosenka niedluga.\nNie dluga, nie krotka, " +
